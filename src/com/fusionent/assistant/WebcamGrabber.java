2d1b0e194f45d3cf4cadc15603fdb4a02628c74b
zachary.kuhn <zachary.kuhn@servicenow.com>
2021-11-18 21:25:26 -0800

    Mac got working -not tested win
    bump deps for javacv
    package EVERYthing using assembly plugin
    add jenv
    add a build and run shorcut using the packaged up artifact
    
    ---
    VideoInputFrameGrabber didn't work on Mac.. saw it was a win only
    thing.. used opencv grabber instead.

--- src/com/fusionent/assistant/WebcamGrabber.java
+++ src/com/fusionent/assistant/WebcamGrabber.java
@@ -1,32 +1,36 @@
 package com.fusionent.assistant;
 
 
-import static org.bytedeco.javacpp.opencv_core.*;
-import static org.bytedeco.javacpp.opencv_imgproc.*;
-import static org.bytedeco.javacpp.opencv_imgcodecs.*;
+import static org.bytedeco.opencv.global.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
 
+import org.bytedeco.opencv.opencv_core.*;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.VideoInputFrameGrabber;
+import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 
 
 public class WebcamGrabber implements Runnable {
     //final int INTERVAL=1000;///you may use interval
-    IplImage image;
     CanvasFrame canvas = new CanvasFrame("Web Cam");
     public WebcamGrabber() {
         canvas.setDefaultCloseOperation(javax.swing.JFrame.DISPOSE_ON_CLOSE);
     }
     @Override
     public void run() {
-        FrameGrabber grabber = new VideoInputFrameGrabber(0); 
-        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+
+        //FrameGrabber grabber = new VideoInputFrameGrabber(0); 
+        //
+        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
+        OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
         int i=0;
         try {
             grabber.start();
-            IplImage img;
+            Mat img;
             Frame frame;
             while (true) {
                 frame = grabber.grab();
@@ -41,6 +45,7 @@ public class WebcamGrabber implements Runnable {
                  //Thread.sleep(INTERVAL);
             }
         } catch (Exception e) {
+            System.out.println("Caught Exception:");
         	System.out.println(e);
         }
     }
